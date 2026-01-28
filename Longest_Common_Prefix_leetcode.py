cb80c2c10eaac8e5d0be46ef79d6c10e99032f80
Netsanet Demis <bereketdemis72@gmail.com>
2026-01-28 22:07:07 +0300

    Implement longest common prefix function

---
+++ Longest_Common_Prefix_leetcode.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        result=""
+        strs.sort()
+        first = strs[0]
+        last = strs[-1]
+        
+        for i in range(min(len(first),len(last))):
+            if first[i] != last[i]:
+                return result
+            result+= first[i]
+        return result
