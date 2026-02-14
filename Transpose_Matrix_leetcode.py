8f00a099e2dcc37dbdb95219c8f1265b074f9f76
Netsanet Demis <bereketdemis72@gmail.com>
2026-02-14 23:03:37 +0300

    Create Transpose_Matrix_leetcode.py

---
+++ Transpose_Matrix_leetcode.py
@@ -0,0 +1,8 @@
+class Solution:
+    def transpose(self, A: List[List[int]]) -> List[List[int]] :
+        R, C = len(A), len(A[0])
+        ans = [[None] * R for _ in range(C)]
+        for r, row in enumerate(A):
+            for c, val in enumerate(row):
+                ans[c][r] = val
+        return ans
