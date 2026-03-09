6f476f5ac41d522e8521a7ce81cc0b3321372406
Netsanet Demis <209745032+netsy-d@users.noreply.github.com>
2026-03-09 15:49:23 +0300

    Implement countPairs function to count valid pairs

---
+++ Count_Pairs_Whose_Sum_is Lestahn_Target_leetcode.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countPairs(self, nums: List[int], target: int) -> int:
+        count= 0
+        n = len(nums)
+        left = 0
+        right=n-1
+        nums.sort()
+        while left < right:
+            if nums[left] + nums[right] <target:
+                count+=right - left
+                left+=1
+            else:
+                right-=1
+        return count
