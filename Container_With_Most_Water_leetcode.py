509adbcc745c53dbac45bfcc06a9435c21250184
Netsanet Demis <209745032+netsy-d@users.noreply.github.com>
2026-03-10 12:44:46 +0300

    Implement maxArea function to calculate water container

---
+++ Container_With_Most_Water_leetcode.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        n = len(height)
+        left = 0
+        right = n-1
+        maxArea=0
+        while left < right:
+            currentArea = min(height[left],height[right])* (right - left)
+            maxArea = max(currentArea,maxArea)
+            if height[left] < height[right]:
+                left+=1
+            else:
+                right-=1
+        return maxArea
