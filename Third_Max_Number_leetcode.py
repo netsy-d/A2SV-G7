f43e2f6e0fa9f9c1bdfaff2ff590fbb0b6ee5fae
Netsanet Demis <209745032+netsy-d@users.noreply.github.com>
2026-03-10 12:02:09 +0300

    Add thirdMax function to find third maximum number
    Implement a solution to find the third maximum number in a list.

---
+++ Third_Max_Number_leetcode.py
@@ -0,0 +1,11 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums.sort(reverse=True)
+        count = 1
+        for i in range(1,len(nums)):
+            if nums[i] != nums[i-1]:
+                count+=1
+            if count ==3:
+                return nums[i]
+        return nums[0]
+            
