aea86b9205a724b96956437802d03db66efd607a
Netsanet Demis <209745032+netsy-d@users.noreply.github.com>
2026-03-11 22:33:52 +0300

    Add maxOperations method for k-sum pairs
    Implement a solution to find the maximum number of k-sum pairs.

---
+++ Maximum_Number_Of_K_Sum_Pairs_leetcode.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        end= len(nums)-1
+        start = count=0 
+        while start < end:
+            if nums[start] + nums[end] == k:
+                count+=1
+                start+=1
+                end-=1
+            elif nums[start] + nums[end] < k :
+                start+=1
+            else:
+                end-=1
+        return count
